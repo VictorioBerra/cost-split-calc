a0d010e01f3ded801e87e680f9f93d6c0518c03a
Victorio Berra <tory.berra@wustl.edu>
2017-09-20 09:56:32 -0500

    ui revamp

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,16 @@ import { CostSplitCalculatorProvider } from '../../providers/cost-split-calculat
 
 import { SummaryPage } from '../summary/summary';
 
+// TODO, where to put these?
+interface ExpenseCard {
+  name: string;
+  expenses: Expense[];
+}
+
+interface Expense {
+  value: number
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -13,13 +23,54 @@ export class HomePage {
   total: number = 0;
   taxRate: number = 7.5;
 
+  expenseCards: ExpenseCard[];
+
   //debug
   additA = 40;
   totalWithTax = 107.5;
 
   //ignoreTax: boolean = false;
   
-  constructor(public navCtrl: NavController, public calcService: CostSplitCalculatorProvider) {}
+  constructor(public navCtrl: NavController, public calcService: CostSplitCalculatorProvider) {
+    this.expenseCards = [
+      {
+        name: 'Person 1',
+        expenses: [
+          {
+            value: 40
+          }
+        ]
+      }
+    ];
+  }
+
+  addNewCard = function() {
+    let nextId = this.expenseCards.length + 1;
+    this.expenseCards.push({
+      name: `Person ${nextId}`,
+      expenses: [
+        {
+          value: 40
+        }
+      ]
+    });
+  }
+
+  addExpense = function(card) {
+    card.expenses.push({
+      value: null
+    });
+  }
+
+  removeExpense = function(card, expenses) {
+
+    // TODO, ask to remove card?
+    // if(expenses.length == 1) {
+    //   this.expenseCards.splice(this.expenseCards.indexOf(card), 1);
+    // }
+
+    expenses.splice(expenses.indexOf(card), 1);
+  }
 
   calculate = function() {
 
